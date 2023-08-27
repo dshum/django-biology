5b99e3c7d44c6d18b5708d8ae59751b602165663
Denis Shumeev <denis-shumeev@yandex.ru>
2023-08-27 18:24:54 +0300

    Created folder frontend and deleted app carrot.

---
+++ frontend/carrot/src/routes/+layout.svelte
@@ -0,0 +1,41 @@
+<script>
+  import "../app.scss";
+</script>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand shadow rounded px-4 text-light" href="/">Admin</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExample07">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/settings">Settings</a>
+        </li>
+      </ul>
+      <form role="search">
+        <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+      </form>
+    </div>
+  </div>
+</nav>
+
+<div class="container">
+  <div class="section my-4">
+    <slot></slot>
+  </div>
+</div>
+
+<style>
+  .navbar-brand {
+    background-color: darkorchid;
+  }
+</style>
---
+++ frontend/carrot/src/routes/about/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+  let title = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, illum?"
+
+  let updateTitle = () => {
+    title = title.toUpperCase()
+  }
+</script>
+
+<h2>{title}</h2>
+
+<input type="text" bind:value={title}>
+
+<button class="btn" on:click={updateTitle}>Push me</button>
+
+<p class="lead">
+  This is a lead paragraph. It stands out from regular paragraphs.
+</p>
+<p class="lead">
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci architecto assumenda autem commodi deserunt eveniet fuga, fugit hic itaque, magnam nobis numquam omnis quia quo ratione soluta sunt. Amet architecto, consequatur debitis doloribus, dolorum eius eligendi error excepturi fugiat harum illum inventore libero nemo officia pariatur placeat possimus quia quisquam repellendus reprehenderit, sed similique suscipit tempora totam veritatis voluptate voluptatum.
+</p>
+
+<div class="my-4">
+  <form>
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Email address</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+      <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputPassword1" class="form-label">Password</label>
+      <input type="password" class="form-control" id="exampleInputPassword1">
+    </div>
+    <div class="mb-3 form-check">
+      <input type="checkbox" class="form-check-input" id="exampleCheck1">
+      <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</div>
