5fd704a669bfa1930c9a04f09e5dd783cac362d7
Denis Shumeev <denis-shumeev@yandex.ru>
2023-12-26 04:42:54 +0300

    Created new frontend folder with vite, vue3, tailwindcss.

---
+++ frontend/carrot/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline bg-purple-200">
+    Hello world!
+  </h1>
+</template>
