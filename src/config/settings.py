4214bd4b4c8e150e14c075215a9da4bcbf0b828b
Denis Shumeev <denis-shumeev@yandex.ru>
2024-12-12 00:47:01 +0300

    Fix

--- src/config/settings.py
+++ src/config/settings.py
@@ -15,7 +15,7 @@ import socket
 import warnings
 from ast import literal_eval
 from datetime import timedelta
-
+from dotenv import load_dotenv
 from corsheaders.defaults import default_methods
 from django.urls import resolve
 from pathlib import Path
@@ -32,6 +32,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
